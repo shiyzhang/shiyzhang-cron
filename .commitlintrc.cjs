1d681cbd8bef6a121b136985306e5e432c9f487a
shiyzhang <zsyou0911@126.com>
2024-03-06 22:04:13 +0800

    Revert "docs: 添readme文件"
    This reverts commit 30a205ad02c842ef07e2e8ba6022c54d7e5a79ec.

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -1,16 +1,12 @@
 const fs = require('fs');
 const path = require('path');
-const {execSync} = require('child_process');
+const { execSync } = require('child_process');
 
-const scopesSrc = fs
-  .readdirSync(path.resolve(__dirname, 'src'), {withFileTypes: true})
+const scopes = fs
+  .readdirSync(path.resolve(__dirname, 'src'), { withFileTypes: true })
   .filter((dirent) => dirent.isDirectory())
   .map((dirent) => dirent.name.replace(/s$/, ''));
-const scopesPackage = fs
-  .readdirSync(path.resolve(__dirname, 'package'), {withFileTypes: true})
-  .filter((dirent) => dirent.isDirectory())
-  .map((dirent) => dirent.name.replace(/s$/, ''));
-const scopes = [...scopesSrc, ...scopesPackage]
+
 // precomputed scope
 const scopeComplete = execSync('git status --porcelain || true')
   .toString()
@@ -71,9 +67,9 @@ module.exports = {
 
     // English
     typesAppend: [
-      {value: 'wip', name: 'wip:      work in process'},
-      {value: 'workflow', name: 'workflow: workflow improvements'},
-      {value: 'types', name: 'types:    type definition file changes'},
+      { value: 'wip', name: 'wip:      work in process' },
+      { value: 'workflow', name: 'workflow: workflow improvements' },
+      { value: 'types', name: 'types:    type definition file changes' },
     ],
 
     // 中英文对照版
@@ -90,20 +86,20 @@ module.exports = {
       confirmCommit: '是否提交或修改commit ?',
     },
     types: [
-      {value: 'feat', name: 'feat:     新增功能'},
-      {value: 'fix', name: 'fix:      修复缺陷'},
-      {value: 'docs', name: 'docs:     文档变更'},
-      {value: 'style', name: 'style:    代码格式'},
-      {value: 'refactor', name: 'refactor: 代码重构'},
-      {value: 'perf', name: 'perf:     性能优化'},
-      {value: 'test', name: 'test:     添加疏漏测试或已有测试改动'},
-      {value: 'build', name: 'build:    构建流程、外部依赖变更 (如升级 npm 包、修改打包配置等)'},
-      {value: 'ci', name: 'ci:       修改 CI 配置、脚本'},
-      {value: 'revert', name: 'revert:   回滚 commit'},
-      {value: 'chore', name: 'chore:    对构建过程或辅助工具和库的更改 (不影响源文件、测试用例)'},
-      {value: 'wip', name: 'wip:      正在开发中'},
-      {value: 'workflow', name: 'workflow: 工作流程改进'},
-      {value: 'types', name: 'types:    类型定义文件修改'},
+      { value: 'feat', name: 'feat:     新增功能' },
+      { value: 'fix', name: 'fix:      修复缺陷' },
+      { value: 'docs', name: 'docs:     文档变更' },
+      { value: 'style', name: 'style:    代码格式' },
+      { value: 'refactor', name: 'refactor: 代码重构' },
+      { value: 'perf', name: 'perf:     性能优化' },
+      { value: 'test', name: 'test:     添加疏漏测试或已有测试改动' },
+      { value: 'build', name: 'build:    构建流程、外部依赖变更 (如升级 npm 包、修改打包配置等)' },
+      { value: 'ci', name: 'ci:       修改 CI 配置、脚本' },
+      { value: 'revert', name: 'revert:   回滚 commit' },
+      { value: 'chore', name: 'chore:    对构建过程或辅助工具和库的更改 (不影响源文件、测试用例)' },
+      { value: 'wip', name: 'wip:      正在开发中' },
+      { value: 'workflow', name: 'workflow: 工作流程改进' },
+      { value: 'types', name: 'types:    类型定义文件修改' },
     ],
     emptyScopesAlias: 'empty:      不填写',
     customScopesAlias: 'custom:     自定义',
